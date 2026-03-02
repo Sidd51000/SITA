6052cdbd0827228fc5f511e5c7bd51b044c95744
Sidd51000 <siddharthnaik20@student.sfit.ac.in>
2026-03-02 22:05:28 +0530

    Added Log-in page

---
+++ app/login/page.tsx
@@ -0,0 +1,23 @@
+import { Playfair_Display, Lato } from "next/font/google";
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-playfair",
+  display: "swap",
+});
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "900"],
+  variable: "--font-lato",
+  display: "swap",
+});
+
+const Page = () => {
+    return (
+     <h1>Register Page</h1>   
+    )
+}
+
+export default Page;
